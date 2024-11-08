e3f8939a5145796a08bf8ceec9ddef25d957d258
kol <kol3wang@gmail.com>
2024-11-07 22:31:32 -0500

    server.ts updated again for render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-import path from 'node:path';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,11 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve static files from the client’s dist directory
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(routes);
 
 db.once('open', () => {
+  console.log(`Process environment PORT: ${process.env.PORT}`);
   app.listen(PORT, () => console.log(`🌍 Now listening on port ${PORT}`));
 });
